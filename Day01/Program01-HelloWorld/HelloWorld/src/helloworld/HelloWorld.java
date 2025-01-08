e7ff55dc8ecf7eb3480b429ce69a1fabe920351c
Tam Nguyen <tamng.tng@gmail.com>
2025-01-08 07:15:02 +0700

    day 01

---
+++ Day01/Program01-HelloWorld/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ *
+ * @author tamnguyen
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Tam Nguyen");
+        System.out.println("5 + 5 = " + 5 + 5);
+        System.out.println("6 +6 = " + (6 + 6));
+    }
+    
+}
